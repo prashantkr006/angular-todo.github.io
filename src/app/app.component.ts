f01f4c5568ea0b400c599731fdbc2ce0f26924d4
prashantkr006 <prashantkr006@gmail.com>
2022-05-01 16:34:37 +0530

    anular todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { AddTaskDialogComponent } from './add-task-dialog/add-task-dialog.component';
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-ToDo';
+
+  todo: any = [
+    { id: 1, value: 'Write blog' },
+    { id: 2, value: 'Do Code' },
+  ];
+
+  constructor(public dialog: MatDialog){};
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddTaskDialogComponent,
+      {
+        width: '500px'
+      });
+
+      dialogRef.afterClosed().subscribe(res => this.addData(res))
+  }
+
+  addData(item: string) {
+    this.todo.push({id: this.todo.length+1 , value: item})
+    console.log(this.todo)
+  }
+
+  removeTask(id: number) {
+    this.todo = this.todo.filter((task: { id: number; }) => task.id !== id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from  '@angular/material/dialog'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatDividerModule} from '@angular/material/divider';
+import { AddTaskDialogComponent } from './add-task-dialog/add-task-dialog.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskDialogComponent
   ],
+  entryComponents: [AddTaskDialogComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
